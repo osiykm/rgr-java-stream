09541cfe772561e650d5c2ef8679c556e24e49ab
Микола Осіюк < >
2016-10-10 13:01:05 +0300

    init

---
+++ src/main/java/osiykm/cg/RgrJavaStreamApplication.java
@@ -0,0 +1,12 @@
+package osiykm.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgrJavaStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgrJavaStreamApplication.class, args);
+	}
+}
---
+++ src/test/java/osiykm/cg/RgrJavaStreamApplicationTests.java
@@ -0,0 +1,16 @@
+package osiykm.cg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RgrJavaStreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
